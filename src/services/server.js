20a8d1976dcd3cf8b8270bffe09f7990c84c5301
Sebastian <sfdelgadop@unal.edu.co>
2020-04-22 14:41:37 -0500

    quick fix

--- src/services/server.js
+++ src/services/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.AUTH_URL
-export const port = process.env.AUTH_PORT
-export const entryPoint = process.env.AUTH_USERS_ENTRY
+export const url = process.env.SERVICES_URL
+export const port = process.env.SERVICES_PORT
+export const entryPoint = process.env.SERVICES_USERS_ENTRY
