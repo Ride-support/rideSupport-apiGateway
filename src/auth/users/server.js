fa11c928aa69608786cecd8e3b237761a839bc2a
Sebastian <sfdelgadop@unal.edu.co>
2020-04-22 23:13:57 -0500

    adding last ipdireccions

--- src/auth/users/server.js
+++ src/auth/users/server.js
@@ -4,6 +4,6 @@ export const entryPoint = process.env.AUTH_USERS_ENTRY
 
 export const endpoint = {
     reservas: {
-        user: "http://172.17.0.1:4001/user",
+        user: "http://54.86.139.0:4001/user",
     }
 }
