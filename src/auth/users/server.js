97aff35a7afb0c6b6cf95735fa888aab47a8ee95
German <gdgarcian@unal.edu.co>
2020-04-25 19:50:01 -0500

    Sirve el AG de reserva

--- src/auth/users/server.js
+++ src/auth/users/server.js
@@ -2,8 +2,10 @@ export const url = process.env.AUTH_URL
 export const port = process.env.AUTH_PORT
 export const entryPoint = process.env.AUTH_USERS_ENTRY
 
+/*
 export const endpoint = {
     reservas: {
-        user: "http://54.86.139.0:4001/user",
+        user: "http://172.17.0.1:4001/user",
     }
 }
+*/
